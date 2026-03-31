f6e93961193d8d8a11fed7920842e7ddc1dc035b
Ava.io <modupeoluwadorcas142@gmail.com>
2026-03-31 15:36:18 +0100

    database connected successfully

--- src/Model/createBudgetTable.js
+++ src/Model/createBudgetTable.js
@@ -1,8 +1,8 @@
 export const createBudgetTable = `
-CREATE TABLE IF NOT EXISTS(
+CREATE TABLE IF NOT EXISTS budget(
 id SERIAL PRIMARY KEY,
 category VARCHAR(225) NOT NULL,
-monthly_limit INT NOR NULL,
+monthly_limit INT NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 `;
